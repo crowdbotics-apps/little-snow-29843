1679e2276ef72e19756cda31199fd0ed188ada7e
Crowdbotics <team@crowdbotics.com>
2021-08-16 03:27:02 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "little_snow_29843Identifier",
+  url: "https://little_snow_29843Identifier.botics.co",
+  api: "https://little_snow_29843Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
